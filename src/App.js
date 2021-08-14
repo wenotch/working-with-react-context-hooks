eb4e2b3442ce9e2f26c8213560a41c8ec971b0a0
Nwanochie Emmanuel <emmanuel.nwanochie.197850@unn.edu.ng>
2021-08-14 19:16:08 +0100

    project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Posts from './components/Posts';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Posts />
+      </header>
+    </div>
+  );
+}
+
+export default App;
